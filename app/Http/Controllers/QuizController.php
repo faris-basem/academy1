58d2af62a1910928cc59a469b15ff3bb88cd7585
faris-basem <faris.2922@gmail.com>
2023-06-06 23:12:40 +0300

    quizzes leaderborde

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Answer;
+use App\Models\Course;
+use App\Models\Lesson;
 use App\Models\Quiz;
 use App\Models\MyQuiz;
 use App\Models\Question;
+use App\Models\Section;
 use App\Models\StudentPoint;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,16 +19,16 @@ class QuizController extends Controller
     {
         $mq = MyQuiz::where('user_id', Auth::guard('api')->user()->id)->where('quiz_id', $request->quiz_id)->first();
         $quiz = Quiz::where('id', $request->quiz_id)->first();
-        if($mq){
-            $quiz['remaining_attempts']=$mq->attempts;
-            $quiz['your_points']=$mq->points;
+        if ($mq) {
+            $quiz['remaining_attempts'] = $mq->attempts;
+            $quiz['your_points'] = $mq->points;
             return response()->json([
                 'message' => 'Data Fetched Successfully',
                 'code' => 200,
                 'status' => true,
                 'data' => $quiz
             ]);
-        }else{
+        } else {
             return response()->json([
                 'message' => 'Data Fetched Successfully',
                 'code' => 200,
@@ -39,8 +42,15 @@ class QuizController extends Controller
     {
         $mq = MyQuiz::where('user_id', Auth::guard('api')->user()->id)->where('quiz_id', $request->quiz_id)->first();
         $quiz = Quiz::where('id', $request->quiz_id)->with('questions.answers')->first();
+        $less = Lesson::where('id', $quiz->lesson_id)->first();
+        $sec = Section::where('id', $less->section_id)->first();
+        $std_points = StudentPoint::where('user_id', Auth::guard('api')->user()->id)->where('course_id', $sec->course_id)->first();
+
         if ($mq) {
             if ($mq->attempts > 0) {
+                $std_points->points = $std_points->points - $mq->points;
+                $std_points->save();
+                $mq->points = 0;
                 $mq->attempts--;
                 $mq->save();
                 return response()->json([
@@ -63,6 +73,14 @@ class QuizController extends Controller
             $new_quiz->attempts = $quiz->attempts;
             $new_quiz->points = 0;
             $new_quiz->save();
+
+            if (!$std_points) {
+                $new_std = new StudentPoint();
+                $new_std->points = $new_quiz->points;
+                $new_std->user_id = Auth::guard('api')->user()->id;
+                $new_std->course_id = $sec->course_id;
+                $new_std->save();
+            }
             return response()->json([
                 'message' => 'Data Fetched Successfully',
                 'code' => 200,
@@ -76,34 +94,44 @@ class QuizController extends Controller
     {
         $mq = MyQuiz::where('user_id', Auth::guard('api')->user()->id)->where('quiz_id', $request->quiz_id)->first();
         $quiz = Quiz::where('id', $request->quiz_id)->first();
-        $questions=Question::where('quiz_id',$request->quiz_id)->pluck('id');
-        $answers=Answer::whereIn('question_id',$questions)->get();
+        $questions = Question::where('quiz_id', $request->quiz_id)->pluck('id');
+        $answers = Answer::whereIn('question_id', $questions)->get();
+        $less = Lesson::where('id', $quiz->lesson_id)->first();
+        $sec = Section::where('id', $less->section_id)->first();
+
         $arrayData = json_decode($request->answers, true);
-        $trueans=0;
+        $trueans = 0;
         foreach ($arrayData as $item) {
             $answerId = $item['answer_id'];
             $answerItem = $answers->where('id', $answerId)->first();
-            if ($answerItem->status==1) {
+            if ($answerItem->status == 1) {
                 $trueans++;
             }
         }
-        $points_for_ans=$quiz->points/$questions->count();
-        if($quiz->attempts==$mq->attempts){
-            $mq->points=$points_for_ans*$trueans;
+        $points_for_ans = $quiz->points / $questions->count();
+
+        $std_points = StudentPoint::where('user_id', Auth::guard('api')->user()->id)->where('course_id', $sec->course_id)->first();
+
+        if ($quiz->attempts == $mq->attempts) {
+            $mq->points = $points_for_ans * $trueans;
             $mq->save();
+            $std_points->points = $std_points->points + $mq->points;
+            $std_points->save();
             return response()->json([
                 'message' => 'Data Fetched Successfully',
                 'code' => 200,
                 'status' => true,
                 'data' => $mq
             ]);
-        }else{
-            $x=$quiz->attempts-$mq->attempts;
-            $deduction=$x*$quiz->deduction_per_attempt;
-            $yourponts=$points_for_ans*$trueans;
-            $finalponts=$yourponts-$deduction;
-            $mq->points=$finalponts;
+        } else {
+            $attempts_count = $quiz->attempts - $mq->attempts;
+            $deduction = $attempts_count * $quiz->deduction_per_attempt;
+            $yourpoints = $points_for_ans * $trueans;
+            $finalpoints = $yourpoints - $deduction;
+            $mq->points = $finalpoints;
             $mq->save();
+            $std_points->points = $std_points->points + $mq->points;
+            $std_points->save();
             return response()->json([
                 'message' => 'Data Fetched Successfully',
                 'code' => 200,
