5138c8f51c9e97388a0b6e3c3de69b1f007193f7
faris-basem <faris.2922@gmail.com>
2023-07-01 14:51:29 +0300

    edit auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\SubjectController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,20 @@ Route::get('/home', function () {
     return view('home');
 });
 
-Route::get('show_subjects', [SubjectController::class, 'show_subjects']);
-Route::get('get_subjects_data', [SubjectController::class, 'get_subjects_data'])->name('get_subjects_data');
-Route::get('subject_levels/{sub}', [SubjectController::class, 'subject_levels']);
 
-Route::post('store_subject',  [SubjectController::class , 'store_subject'])->name('store_subject');
-Route::post('delete_subject',  [SubjectController::class , 'delete_subject'])->name('delete_subject');
+
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::get('show_subjects', [SubjectController::class, 'show_subjects']);
+    Route::get('get_subjects_data', [SubjectController::class, 'get_subjects_data'])->name('get_subjects_data');
+    Route::get('subject_levels/{sub}', [SubjectController::class, 'subject_levels']);
+
+    Route::post('store_subject',  [SubjectController::class , 'store_subject'])->name('store_subject');
+    Route::post('delete_subject',  [SubjectController::class , 'delete_subject'])->name('delete_subject');
+
+    
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
