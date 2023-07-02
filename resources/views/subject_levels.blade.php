6d4db62f26075f5d0eb7f30f2bb73523e4f9ec23
faris-basem <faris.2922@gmail.com>
2023-07-02 11:16:34 +0300

    edit sidebar

--- resources/views/subject_levels.blade.php
+++ resources/views/subject_levels.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.master')
 
 @section('css')
-<link rel="stylesheet" href="//cdn.datatables.net/1.11.2/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('/app-assets/vendors/css/tables/datatable/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/app-assets/vendors/css/tables/datatable/responsive.bootstrap4.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/app-assets/vendors/css/tables/datatable/buttons.bootstrap4.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/app-assets/vendors/css/tables/datatable/rowGroup.bootstrap4.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/app-assets/css-rtl/plugins/extensions/ext-component-sweet-alerts.css')}}">
 @endsection
 
 @section('title')
@@ -70,7 +75,17 @@
     
     </script>
     
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="//cdn.datatables.net/1.11.2/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
+    <script src="{{asset('/app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/tables/datatable/responsive.bootstrap4.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/tables/datatable/datatables.checkboxes.min.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/tables/datatable/datatables.buttons.min.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/tables/datatable/dataTables.rowGroup.min.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js')}}"></script>
+    <script src="{{asset('/app-assets/vendors/js/extensions/sweetalert2.all.min.js')}}"></script>
+    <script src="{{asset('/app-assets/js/scripts/extensions/ext-component-sweet-alerts.js')}}"></script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ Route::get('/home', function () {
 
 Route::group(['middleware' => ['auth']], function () {
 
-    Route::get('show_subjects', [SubjectController::class, 'show_subjects']);
+    Route::get('show_subjects', [SubjectController::class, 'show_subjects'])->name('show_subjects');
     Route::get('get_subjects_data', [SubjectController::class, 'get_subjects_data'])->name('get_subjects_data');
-    Route::get('subject_levels/{sub}', [SubjectController::class, 'subject_levels']);
-
     Route::post('store_subject', [SubjectController::class , 'store_subject' ])->name('store_subject' );
     Route::post('edit_subject',  [SubjectController::class , 'edit_subject'  ])->name('edit_subject'  );
     Route::post('delete_subject',[SubjectController::class , 'delete_subject'])->name('delete_subject');
+    Route::get('subject_levels/{sub}', [SubjectController::class, 'subject_levels']);
+    
+    Route::get('show_levels', [SubjectController::class, 'show_levels'])->name('show_levels');
 
     
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
