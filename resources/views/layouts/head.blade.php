d98283d07c1faa3c976f1ee04fd3003de33dd7fa
faris-basem <faris.2922@gmail.com>
2023-07-02 08:32:52 +0300

    dashboard font + logout button in navbar

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -36,6 +36,14 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/custom-rtl.css')}}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style-rtl.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+    <style>
+        *{
+            font-family: 'Cairo', sans-serif;
+        }
+    </style>
     <!-- END: Custom CSS-->
     @yield('css')
 </head>
